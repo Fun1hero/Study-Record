a2e0a24050903247b9b426541e6f79163e090f79
Misha <misha@omac-2.local>
2018-03-25 15:08:27 +0300

    Added Bday to csv shown in ruby

--- main.rb
+++ main.rb
@@ -4,15 +4,14 @@ puts "Second commit"
 puts "Hello Dima"
 name = []
 marks = []
+bday = []
 filename = 'Marks.csv'
 i = 0
 CSV.foreach(filename) do |row|
   name.push(row[0])
   marks.push(row[1])
-  puts "#{name[i]} , #{marks[i]}"
+  bday.push(row[2])
+  puts "#{name[i]} -  #{marks[i]} - #{bday[i]}"
  # puts $..to_i
   i += 1
-end
-puts name
-puts marks
-puts 
+end 
