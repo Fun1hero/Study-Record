b657b796989e2b14381a7478a8bef487e3ab56c5
Misha <misha@omac-2.local>
2018-03-25 12:50:10 +0300

    New

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ puts "first commit"
 puts "Second commit"
 puts "Hello Dima"
 require 'csv'
-CSV.open("marks.csv","w") do |wr|
-   wr << ["Fifthgreat", "Marks", ]
+CSV.open("marks.csv","r") do |read|
+   read << ["Fifthgreat", "Marks" ]
 end
 
