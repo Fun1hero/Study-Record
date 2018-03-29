3088660fd011c037bbfcd1760dbb5e1b54c3beb0
Misha <misha@omac-2.local>
2018-03-29 21:48:33 +0300

    List of grades

--- 2main.rb
+++ 2main.rb
@@ -1,3 +1,16 @@
 require 'json'
   
-  file =  
+  file = File.read('marks.json') 
+  marks_hash = JSON.parse(file)
+ # puts marks_hash.size
+ # puts marks_hash['FithsGrade']
+  marks_hash['FithsGrade'].map{ |people| 
+    puts "#{people['name']} (#{people['bDay']}) -> #{people['mark']}"
+  }
+  
+ # marks_hash['FithsGrade'] = ['name'=>'Vasya Pupkin','mark'=>[4,5,6,7,8,8]]
+
+ # File.open('marks.json','w') { |f| 
+  #  f.write(marks_hash.to_json)
+ # }
+
