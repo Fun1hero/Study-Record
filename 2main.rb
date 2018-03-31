b45419c378ae1a9e13e243b914c4d4995aefac69
Yuri Kabanov <y.kabanov@gmail.com>
2018-03-31 16:29:59 +0300

    simple code: add hash

--- 2main.rb
+++ 2main.rb
@@ -1,5 +1,57 @@
 require 'json'
+
+# init hash
+=begin
+  peoples = Hash.new
+  peoples[1] = {:name => "Dyno", :marks => [10,11,10,12]}
+  peoples[2] = {:name => "Myno", :marks => [12,11,12,12]}
+  peoples[3] = {:name => "Zyno", :marks => [9,11,12,9]}
+=end
+
+  file = File.read('peoples.json') 
+  peoples = JSON.parse(file)
+
+  system "clear"
+
+  def list_of_peoples(peoples_hash)
+    puts "List of peoples:"
+    peoples_hash.map{|id, people|
+      puts "#{id}. #{people["name"]}: #{people["marks"]}"
+    }
+  end
+
+  list_of_peoples(peoples)
+
+  puts ""
+  puts "If you want to change name of people, than enter : \"ID New_Name\""
+
+  new_name = gets.chomp
+
+  if new_name.length > 3 then 
+
+    id = new_name.split(" ")[0]
+    name = new_name.split(" ")[1]
+    peoples[id][:name] = name
+
+# open file or create if not exist
+    File.open('peoples.json','w') { |f| f.write(peoples.to_json) }
+# reread file
+    file = File.read('peoples.json') 
+    peoples = JSON.parse(file)
   
+  else 
+
+    puts "Nothing was changed !"
+    puts ""
+
+  end
+
+  list_of_peoples(peoples)
+
+
+
+=begin
+
   file = File.read('marks.json') 
   marks_hash = JSON.parse(file)
  # puts marks_hash.size
@@ -13,4 +65,6 @@ require 'json'
  # File.open('marks.json','w') { |f| 
   #  f.write(marks_hash.to_json)
  # }
+ #
+=end
 
